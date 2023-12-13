be4c47a9e8427a58c0a49bae17c56c3d783adcdd
Ignacio Chavez <nacho_ch3@hotmail.com>
2023-12-13 19:36:00 -0300

    primer avance

---
+++ js/app-login.js
@@ -0,0 +1,14 @@
+function validarForm() {
+    var usuario = document.getElementById("usuario").value;
+    var contrasena = document.getElementById("contrasena").value;
+
+    // Comprueba si el usuario y la contraseña son correctos
+    if (usuario === "MuroDigital25" && contrasena === "CCTVdigital") {
+        // Redirecciona al otro archivo HTML
+        window.location.href = "html/store.html";
+        return false;
+    } else {
+        alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+        return false;
+    }
+}
