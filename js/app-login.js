2778bcf04f3759d9ace56c1521f9b4b544e27748
Ignacio Chavez <nacho_ch3@hotmail.com>
2023-12-13 21:58:58 -0300

    Añadido dashboard

--- js/app-login.js
+++ js/app-login.js
@@ -5,7 +5,7 @@ function validarForm() {
     // Comprueba si el usuario y la contraseña son correctos
     if (usuario === "MuroDigital25" && contrasena === "CCTVdigital") {
         // Redirecciona al otro archivo HTML
-        window.location.href = "html/store.html";
+        window.location.href = "dashboard.html";
         return false;
     } else {
         alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
